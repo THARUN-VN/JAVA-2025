533bad505f4f6066d9148c3b6ef53c305250d96c
THARUN V N <eng24am0102@dsu.edu.in>
2025-10-16 11:01:10 +0530

    java model 2 and some change to exsiting codes

--- square.java
+++ square.java
@@ -1,12 +1,11 @@
-import java.util.Scanner;
-
-public class square {
-    public static void main(String[] args)
-    {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("enter a number :");
-        int n=sc.nextInt();
-         System.out.println("square is :"+n*n);
-        sc.close();
+import java.util.*;
+class Square {
+    public static void main (String[] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter a numbers: ");
+        int a=sc.nextInt();
+        System.out.print("Square of a number "+a+ " is: ");
+       System.out.println(a*a);
     }
+    
 }
